680abf20748bb7086b49bc9d921ce59d4e0a62f4
Linfye <3158203624@qq.com>
2024-04-13 22:24:31 +0800

    Add MainMenu and LevelMenu

---
+++ Scripts/LevelMenu.gd
@@ -0,0 +1,31 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_return_pressed():
+	get_tree().change_scene_to_file("res://Scenes//main_menu.tscn")
+
+
+func _on_intro_pressed():
+	get_tree().change_scene_to_file("res://Scenes//Levels//S1.tscn")
+
+
+func _on_zeno_pressed():
+	get_tree().change_scene_to_file("res://Scenes//Levels//S2.tscn")
+
+
+func _on_limit_pressed():
+	get_tree().change_scene_to_file("res://Scenes//Levels//S3.tscn")
+
+
+func _on_circle_pressed():
+	get_tree().change_scene_to_file("res://Scenes//Levels//S4.tscn")
---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,20 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes//level_menu.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
